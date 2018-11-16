d7a1b279ac6758d64f46f6284fd9ea08dad0f958
Maxym Marina <maxymmrn@gmail.com>
2018-11-16 09:12:01 +0200

    Code for Lab3 (Bubble Sort)

---
+++ Lab3_Marina.cpp
@@ -0,0 +1,6 @@
+﻿1>------ Build started: Project: Lab3_Marina, Configuration: Debug Win32 ------
+1>Lab3_Marina.cpp
+1>c:\users\максим маріна\desktop\lab\lab3\lab3_marina\lab3_marina\lab3_marina.cpp(27): error C2664: 'void PrintArr(int [][5])': cannot convert argument 1 from 'int' to 'int [][5]'
+1>c:\users\максим маріна\desktop\lab\lab3\lab3_marina\lab3_marina\lab3_marina.cpp(27): note: Conversion from integral type to pointer type requires reinterpret_cast, C-style cast or function-style cast
+1>Done building project "Lab3_Marina.vcxproj" -- FAILED.
+========== Build: 0 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
